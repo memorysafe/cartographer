078db98e1a89f4e0c127bd0f3bd9aaa88568fc0f
Jianxiong Cai <caijx@shanghaitech.edu.cn>
2017-09-04 15:44:20 +0800

    submap-converter: under developement, just working now

--- cartographer/io/proto_stream.h
+++ cartographer/io/proto_stream.h
@@ -22,7 +22,7 @@
 #include "cartographer/common/port.h"
 
 namespace cartographer {
-namespace io {
+    namespace io {
 
 // A simple writer of a compressed sequence of protocol buffer messages to a
 // file. The format is not intended to be compatible with any other format used
@@ -30,56 +30,56 @@ namespace io {
 //
 // TODO(whess): Compress the file instead of individual messages for better
 // compression performance? Should we use LZ4?
-class ProtoStreamWriter {
- public:
-  ProtoStreamWriter(const string& filename);
-  ~ProtoStreamWriter();
+        class ProtoStreamWriter {
+        public:
+            ProtoStreamWriter(const string& filename);
+            ~ProtoStreamWriter();
 
-  ProtoStreamWriter(const ProtoStreamWriter&) = delete;
-  ProtoStreamWriter& operator=(const ProtoStreamWriter&) = delete;
+            ProtoStreamWriter(const ProtoStreamWriter&) = delete;
+            ProtoStreamWriter& operator=(const ProtoStreamWriter&) = delete;
 
-  // Serializes, compressed and writes the 'proto' to the file.
-  template <typename MessageType>
-  void WriteProto(const MessageType& proto) {
-    string uncompressed_data;
-    proto.SerializeToString(&uncompressed_data);
-    Write(uncompressed_data);
-  }
+            // Serializes, compressed and writes the 'proto' to the file.
+            template <typename MessageType>
+            void WriteProto(const MessageType& proto) {
+                string uncompressed_data;
+                proto.SerializeToString(&uncompressed_data);
+                Write(uncompressed_data);
+            }
 
-  // This should be called to check whether writing was successful.
-  bool Close();
+            // This should be called to check whether writing was successful.
+            bool Close();
 
- private:
-  void Write(const string& uncompressed_data);
+        private:
+            void Write(const string& uncompressed_data);
 
-  std::ofstream out_;
-};
+            std::ofstream out_;
+        };
 
 // A reader of the format produced by ProtoStreamWriter.
-class ProtoStreamReader {
- public:
-  ProtoStreamReader(const string& filename);
-  ~ProtoStreamReader();
+        class ProtoStreamReader {
+        public:
+            ProtoStreamReader(const string& filename);
+            ~ProtoStreamReader();
 
-  ProtoStreamReader(const ProtoStreamReader&) = delete;
-  ProtoStreamReader& operator=(const ProtoStreamReader&) = delete;
+            ProtoStreamReader(const ProtoStreamReader&) = delete;
+            ProtoStreamReader& operator=(const ProtoStreamReader&) = delete;
 
-  template <typename MessageType>
-  bool ReadProto(MessageType* proto) {
-    string decompressed_data;
-    return Read(&decompressed_data) &&
-           proto->ParseFromString(decompressed_data);
-  }
+            template <typename MessageType>
+            bool ReadProto(MessageType* proto) {
+                string decompressed_data;
+                return Read(&decompressed_data) &&
+                       proto->ParseFromString(decompressed_data);
+            }
 
-  bool eof() const;
+            bool eof() const;
 
- private:
-  bool Read(string* decompressed_data);
+        private:
+            bool Read(string* decompressed_data);
 
-  std::ifstream in_;
-};
+            std::ifstream in_;
+        };
 
-}  // namespace io
+    }  // namespace io
 }  // namespace cartographer
 
 #endif  // CARTOGRAPHER_IO_PROTO_STREAM_H_
